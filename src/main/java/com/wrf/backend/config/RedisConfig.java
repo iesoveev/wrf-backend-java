be5979c255eb548f06e4c75d49a606357c29f1d8
iesoveev <esoveevi@taptaxi.su>
2020-02-07 17:04:44 +0400

    new auth with redis

---
+++ src/main/java/com/wrf/backend/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.wrf.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+}
