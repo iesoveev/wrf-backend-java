1e71138a80e45cb2796cc47d5a600ad2885926c7
iesoveev <esoveevi@taptaxi.su>
2020-02-07 17:31:01 +0400

    new auth with redis

--- src/main/java/com/wrf/backend/config/RedisConfig.java
+++ src/main/java/com/wrf/backend/config/RedisConfig.java
@@ -1,8 +1,12 @@
 package com.wrf.backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+@Configuration
+@EnableRedisRepositories("com.wrf.backend.redis.repository")
 public class RedisConfig {
 
     @Bean
