e72c0c5b7dd6361c05bfe1a7f6505d32f87823da
iesoveev <esoveevi@taptaxi.su>
2020-10-05 00:25:14 +0400

    fix swagger authorization

--- src/main/java/com/wrf/backend/config/SwaggerConfig.java
+++ src/main/java/com/wrf/backend/config/SwaggerConfig.java
@@ -2,23 +2,26 @@ package com.wrf.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Collections;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     private ApiKey apiKey() {
-        return new ApiKey("apiKey", "Authorization", "header");
+        return new ApiKey("JWT", "Authorization", "header");
     }
 
     private ApiInfo apiInfo() {
@@ -30,10 +33,21 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
-                .build()
+                .apis(RequestHandlerSelectors.basePackage("com.wrf.backend.controller")).build()
                 .apiInfo(apiInfo())
-                .securitySchemes(Collections.singletonList(apiKey()));
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(singletonList(
+                        SecurityContext.builder()
+                                .securityReferences(
+                                        singletonList(SecurityReference.builder()
+                                                .reference("JWT")
+                                                .scopes(new AuthorizationScope[0])
+                                                .build()
+                                        )
+                                )
+                                .build())
+                );
     }
+
+
 }
